17e8b7ba11a5c696e3a99cdbcc3c86f5abc628e4
a27657060 <a27657060@gmail.com>
2023-08-03 14:29:48 +0800

    router init

--- app.js
+++ app.js
@@ -6,6 +6,43 @@ app.get('/', (req, res) => {
   res.send("hello world")
 })
 
+//取得全餐廳清單
+app.get('/lists', (req, res) => {
+  res.send("get all lists")
+})
+
+//取得新增清單頁
+app.get('/lists/new', (req, res) => {
+  res.send("create new list page")
+})
+
+//新增一家餐聽
+app.post('/lists', (req, res) => {
+  res.send("create new list")
+})
+
+//取得一家餐廳頁面
+app.get('/lists/:id', (req, res) => {
+  res.send(`here is list:${req.params.id}`)
+})
+
+//取得修改一家餐廳頁面
+app.get('/lists/:id/edit', (req, res) => {
+  res.send(`updating list page: ${req.params.id}`)
+})
+
+//修改一家餐廳頁面內容
+app.put('/todos', (req, res) => {
+  res.send(`updating list ${req.params.id}`)
+})
+
+//刪除一家資訊
+app.delete('/todos', (req, res) => {
+  res.send('delete list.')
+})
+
+
+
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
 })
